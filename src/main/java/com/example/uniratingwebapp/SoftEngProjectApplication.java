3ba94a6cdfbddb20801b4376900af5cdbe072c49
Luis Kateshi <luis.kateshi@fti.edu.al>
2023-12-14 20:08:51 +0100

    Initial Commit (spring boot project initialization)

---
+++ src/main/java/com/example/uniratingwebapp/SoftEngProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.uniratingwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftEngProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftEngProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/uniratingwebapp/SoftEngProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uniratingwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftEngProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
