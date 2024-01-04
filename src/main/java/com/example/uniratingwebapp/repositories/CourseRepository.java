1af5194194bdfd07a718808dd77d9752d80abeb7
Luis Kateshi <luis.kateshi@fti.edu.al>
2024-01-04 17:19:17 +0100

    Changes regarding the search for courses by title functionality and Course Details

--- src/main/java/com/example/uniratingwebapp/repositories/CourseRepository.java
+++ src/main/java/com/example/uniratingwebapp/repositories/CourseRepository.java
@@ -2,7 +2,11 @@ package com.example.uniratingwebapp.repositories;
 
 import com.example.uniratingwebapp.entities.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
-
+    @Query("SELECT c FROM Course c WHERE c.Title LIKE %?1%")
+    public List<Course> findByTitle(String searchTerm);
 }
